e52c66f7b31f0a1f6ac20fe6261bfafc0e9b6bcc
Matt Webb <matt531320.efreak@gmail.com>
2019-02-20 15:30:26 -0800

    Adds environment loader code

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,17 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { environmentLoader as environmentLoaderPromise } from './environments/environmentLoader';
 
-if (environment.production) {
-  enableProdMode();
-}
+environmentLoaderPromise.then(env => {
+  if (env.production) {
+    enableProdMode();
+  }
+  environment.ApiUrl = env.ApiUrl;
+  environment.AppName = env.AppName;
+
+  platformBrowserDynamic().bootstrapModule(AppModule);
+});
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
