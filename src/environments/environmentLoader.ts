02d32c21a0b7b3fc8237ab4e9968f6a78c2b1edb
Matt Webb <matt531320.efreak@gmail.com>
2019-02-20 21:44:04 -0800

    This should fix site environment config + updated packages

--- src/environments/environmentLoader.ts
+++ src/environments/environmentLoader.ts
@@ -1,15 +1,29 @@
 import { environment as defaultEnvironment } from './environment';
 
-export const environmentLoader = new Promise<any>((resolve, reject) => {
+class EnvironmentClass {
+  AppName = defaultEnvironment.AppName;
+  ApiUrl = defaultEnvironment.ApiUrl;
+  production = defaultEnvironment.production;
+
+  set(AppName: string, ApiUrl: string, production: boolean) {
+    this.AppName = AppName;
+    this.ApiUrl = ApiUrl;
+    this.production = production;
+  }
+}
+
+export const environmentLoader = new Promise<EnvironmentClass>((resolve, reject) => {
   const xmlhttp = new XMLHttpRequest();
   const method = 'GET';
   const url = './assets/environments/environment.json';
+
   xmlhttp.open(method, url, true);
   xmlhttp.onload = () => {
     if (xmlhttp.status === 200) {
-      resolve(JSON.parse(xmlhttp.responseText));
+      const x: EnvironmentClass = JSON.parse(xmlhttp.responseText);
+      resolve(x);
     } else {
-      resolve(defaultEnvironment);
+      resolve(new EnvironmentClass());
     }
   };
   xmlhttp.send();
--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { environmentLoader as environmentLoaderPromise } from './environments/environmentLoader';
+import { environmentLoader } from './environments/environmentLoader';
 
-environmentLoaderPromise.then(env => {
+environmentLoader.then(env => {
   if (env.production) {
     enableProdMode();
   }
+
   environment.ApiUrl = env.ApiUrl;
   environment.AppName = env.AppName;
 
-  platformBrowserDynamic().bootstrapModule(AppModule);
+  platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err));
 });
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
