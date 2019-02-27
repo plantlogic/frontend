a00aa3561e794d8a3c7a85a3ef89ba588d977e8f
William Hutt <whutt1994@gmail.com>
2019-02-27 13:43:23 -0800

    Style css
    .scss was .css for some reason, changed back

--- src/app/edituser/edituser.component.ts
+++ src/app/edituser/edituser.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-edituser',
   templateUrl: './edituser.component.html',
-  styleUrls: ['./edituser.component.css']
+  styleUrls: ['./edituser.component.scss']
 })
 export class EdituserComponent implements OnInit {
 
