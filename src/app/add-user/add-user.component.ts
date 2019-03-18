b8a27e06dca56071f87d50a880113f7fd3b9f4f4
Matt Webb <hello@mattwebb.io>
2019-03-18 14:52:27 -0700

    Basic working permissions flow

--- src/app/add-user/add-user.component.ts
+++ src/app/add-user/add-user.component.ts
@@ -1,8 +1,9 @@
+import { PlRole } from './../_dto/user/pl-role.enum';
 import { AlertService } from './../_interact/alert.service';
 import { UserService } from './../_api/user.service';
 import { Component, OnInit } from '@angular/core';
 import { TitleService } from '../_interact/title.service';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
 import { User } from '../_dto/user/user';
 
 @Component({
@@ -14,14 +15,19 @@ export class AddUserComponent implements OnInit {
   form: FormGroup;
   submitAttempted = false;
   password: string;
+  plRole = PlRole;
+  roleList: Array<string>;
 
   constructor(private titleService: TitleService, private fb: FormBuilder, private userService: UserService) {
 
     this.form = this.fb.group({
       username: ['', [Validators.required, Validators.minLength(4)]],
       email: ['', [Validators.required, Validators.email]],
-      realname: ['', Validators.required]
+      realname: ['', Validators.required],
+      roles: this.fb.array(this.getRoleBoolArray())
     });
+
+    this.roleList = this.getRoles();
   }
 
   ngOnInit() {
@@ -37,7 +43,11 @@ export class AddUserComponent implements OnInit {
 
     } else {
       this.submitAttempted = false;
-      const user = (new User()).infoConstruct(this.form.value.email, this.form.value.username, this.form.value.realname, []);
+
+      const user = (
+        new User()).infoConstruct(this.form.value.email, this.form.value.username, this.form.value.realname, this.getSelectedRoles()
+      );
+
       this.userService.addUser(user).subscribe(
         data => {
           if (data.success) {
@@ -53,5 +63,29 @@ export class AddUserComponent implements OnInit {
         }
       );
     }
+  }
+
+  getRoles(): Array<string> {
+    const keys = Object.keys(this.plRole);
+    return keys.slice(keys.length / 2);
+  }
+
+  getRoleBoolArray(): Array<boolean> {
+    const out: Array<boolean> = [];
+    for (let i = 0; i < (Object.keys(this.plRole).length) / 2; i++) {
+      out.push(false);
+    }
+    return out;
+  }
+
+  getSelectedRoles(): Array<PlRole> {
+    /* const perms: Array<PlRole> = [];
+    for (let i = 0; i < this.roleList.length; i++) {
+      if ((this.form.get('roles').value)[i]) {
+        perms.push(PlRole[this.roleList[i]]);
+      }
+    }
+    return perms; */
+    return Object.keys(this.plRole).filter((d, ind) => this.form.get('roles').value[ind]).map(key => PlRole[key]);
   }
 }
