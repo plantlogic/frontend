3977f0f96d52046e86762767a67b8ce221e5001d
Matt Webb <hello@mattwebb.io>
2019-03-19 20:16:35 -0700

    Variables need to be exported for envsubst to work

--- docker/environment-config.sh
+++ docker/environment-config.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+
 if [[ -n "${APP_URL}" ]]; then
-  FRONTEND_URL="${APP_URL}"
-  API_URL="${APP_URL}/api"
+  export FRONTEND_URL="${APP_URL}"
+  export API_URL="${APP_URL}/api"
 fi
+
 envsubst '$REDIRECT_URL $FRONTEND_URL $API_URL' < /etc/nginx/conf.d/site.template > /etc/nginx/conf.d/default.conf
 envsubst '$API_URL $APP_NAME' < /etc/nginx/conf.d/environment.template > /usr/share/nginx/html/assets/environments/environment.json
 exec nginx -g 'daemon off;'
