2df04143ed06d4d1892269c5db7f07efde384708
Matt Webb <hello@mattwebb.io>
2019-04-04 19:16:50 -0700

    Data service integration

---
+++ src/app/_dto/card/irrigation-entry.ts
@@ -0,0 +1,10 @@
+export class IrrigationEntry {
+  // fertilizer	string
+  fertilizer: string;
+  // gallons	number($float)
+  gallons: number;
+  // method	string
+  method: string;
+  // workDate	string($date-time)
+  workDate: number;
+}
---
+++ src/app/_dto/card/tractor-entry.spec.ts
@@ -0,0 +1,7 @@
+import { TractorEntry } from './tractor-entry';
+
+describe('TractorEntry', () => {
+  it('should create an instance', () => {
+    expect(new TractorEntry()).toBeTruthy();
+  });
+});
