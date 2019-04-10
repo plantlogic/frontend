32022bd9d6e215c6c414a97143d3709d624183e3
Matt Webb <hello@mattwebb.io>
2019-04-10 00:20:30 -0700

    Minor editing capabilities - needs massive improvements

--- src/app/card/management/open/open-card-data.component.ts
+++ src/app/card/management/open/open-card-data.component.ts
@@ -24,6 +24,7 @@ export class OpenCardDataComponent implements OnInit {
   untouchedCard: Card;
   card: Card;
   editable: boolean;
+  editing = false;
 
   ngOnInit() {
     this.titleService.setTitle('View Card');
@@ -50,72 +51,117 @@ export class OpenCardDataComponent implements OnInit {
   }
 
   private deleteCard() {
-    if (this.editable) {
-      const newAlert = new Alert();
-      newAlert.color = 'danger';
-      newAlert.title = 'WARNING: Deleting Card Permanently';
-      newAlert.message = 'Are you sure you want to delete this card? This action cannot be reversed.';
-      newAlert.timeLeft = undefined;
-      newAlert.blockPageInteraction = true;
-      newAlert.actionName = 'Permanently Delete';
-      newAlert.action$ = new EventEmitter<null>();
-      newAlert.subscribedAction$ = newAlert.action$.subscribe(() => {
-        this.cardEdit.deleteCard(this.card.id).subscribe(data => {
-            if (data.success) {
-              AlertService.newBasicAlert('Card deleted successfully!', false);
-              this.nav.goBack();
-            } else {
-              AlertService.newBasicAlert('Error: ' + data.error, true);
-            }
-          },
-          failure => {
-            AlertService.newBasicAlert('Connection Error: ' + failure.message + ' (Try Again)', true);
-          });
-      });
+    const newAlert = new Alert();
+    newAlert.color = 'danger';
+    newAlert.title = 'WARNING: Deleting Card Permanently';
+    newAlert.message = 'Are you sure you want to delete this card? This action cannot be reversed.';
+    newAlert.timeLeft = undefined;
+    newAlert.blockPageInteraction = true;
+    newAlert.actionName = 'Permanently Delete';
+    newAlert.actionClosesAlert = true;
+    newAlert.action$ = new EventEmitter<null>();
+    newAlert.subscribedAction$ = newAlert.action$.subscribe(() => {
+      this.cardEdit.deleteCard(this.card.id).subscribe(data => {
+          if (data.success) {
+            AlertService.newBasicAlert('Card deleted successfully!', false);
+            this.nav.goBack();
+          } else {
+            AlertService.newBasicAlert('Error: ' + data.error, true);
+          }
+        },
+        failure => {
+          AlertService.newBasicAlert('Connection Error: ' + failure.message + ' (Try Again)', true);
+        });
+    });
 
-      AlertService.newAlert(newAlert);
-    } else {
-      AlertService.newBasicAlert('You don\'t have permission to delete cards.', true);
-    }
+    AlertService.newAlert(newAlert);
   }
 
   private toggleCard() {
-    if (this.editable) {
-      const newAlert = new Alert();
-      newAlert.color = 'primary';
-      if (this.card.isClosed) {
-        newAlert.title = 'Reopen Card';
-        newAlert.message = 'This will reopen a closed card, allowing it to be edited again. Continue?';
-        newAlert.actionName = 'Reopen';
-      } else {
-        newAlert.title = 'Close Card';
-        newAlert.message = 'This will close the card, removing it from the "entry" tab. Continue?';
-        newAlert.actionName = 'Close Card';
-      }
-      newAlert.timeLeft = undefined;
-      newAlert.blockPageInteraction = true;
-      newAlert.closeName = 'Cancel';
-      newAlert.action$ = new EventEmitter<null>();
-      newAlert.subscribedAction$ = newAlert.action$.subscribe(() => {
-        this.untouchedCard.isClosed = !this.untouchedCard.isClosed;
-        this.cardEdit.updateCard(this.untouchedCard).subscribe(data => {
-            if (data.success) {
-              this.card.isClosed = !this.card.isClosed;
-              AlertService.newBasicAlert('Card Edited Successfully!', false);
-            } else {
-              this.untouchedCard.isClosed = !this.untouchedCard.isClosed;
-              AlertService.newBasicAlert('Error: ' + data.error, true);
-            }
-          },
-          failure => {
-            this.untouchedCard.isClosed = !this.untouchedCard.isClosed;
-            AlertService.newBasicAlert('Connection Error: ' + failure.message + ' (Try Again)', true);
-          });
-      });
-
-      AlertService.newAlert(newAlert);
+    const newAlert = new Alert();
+    newAlert.color = 'primary';
+    if (this.card.isClosed) {
+      newAlert.title = 'Reopen Card';
+      newAlert.message = 'This will reopen a closed card, allowing it to be edited again. Continue?';
+      newAlert.actionName = 'Reopen';
     } else {
-      AlertService.newBasicAlert('You don\'t have permission to delete cards.', true);
+      newAlert.title = 'Close Card';
+      newAlert.message = 'This will close the card, removing it from the "entry" tab. Continue?';
+      newAlert.actionName = 'Close Card';
     }
+    newAlert.actionClosesAlert = true;
+    newAlert.timeLeft = undefined;
+    newAlert.blockPageInteraction = true;
+    newAlert.closeName = 'Cancel';
+    newAlert.action$ = new EventEmitter<null>();
+    newAlert.subscribedAction$ = newAlert.action$.subscribe(() => {
+      this.untouchedCard.isClosed = !this.untouchedCard.isClosed;
+      this.cardEdit.updateCard(this.untouchedCard).subscribe(data => {
+          if (data.success) {
+            this.card.isClosed = !this.card.isClosed;
+            AlertService.newBasicAlert('Change saved successfully!', false);
+          } else {
+            this.untouchedCard.isClosed = !this.untouchedCard.isClosed;
+            AlertService.newBasicAlert('Error: ' + data.error, true);
+          }
+        },
+        failure => {
+          this.untouchedCard.isClosed = !this.untouchedCard.isClosed;
+          AlertService.newBasicAlert('Connection Error: ' + failure.message + ' (Try Again)', true);
+        });
+    });
+
+    AlertService.newAlert(newAlert);
+  }
+
+  private toggleEditing(): void {
+    this.editing = !this.editing;
+  }
+
+  private clearChanges(): void {
+    const newAlert = new Alert();
+    newAlert.color = 'warning';
+    newAlert.title = 'Clear Changes';
+    newAlert.message = 'This will clear all changes made to the card, and cannot be undone. Continue?';
+    newAlert.actionName = 'Clear';
+    newAlert.actionClosesAlert = true;
+    newAlert.timeLeft = undefined;
+    newAlert.blockPageInteraction = true;
+    newAlert.closeName = 'Cancel';
+    newAlert.action$ = new EventEmitter<null>();
+    newAlert.subscribedAction$ = newAlert.action$.subscribe(() => {
+      this.card = (new Card()).copyConstructor(this.untouchedCard);
+      this.toggleEditing();
+    });
+
+    AlertService.newAlert(newAlert);
+  }
+
+  private saveChanges(): void {
+    const newAlert = new Alert();
+    newAlert.color = 'warning';
+    newAlert.title = 'Save Card';
+    newAlert.message = 'This will save the card, overwriting what is in the database. This cannot be undone. Continue?';
+    newAlert.actionName = 'Save';
+    newAlert.actionClosesAlert = true;
+    newAlert.timeLeft = undefined;
+    newAlert.blockPageInteraction = true;
+    newAlert.closeName = 'Cancel';
+    newAlert.action$ = new EventEmitter<null>();
+    newAlert.subscribedAction$ = newAlert.action$.subscribe(() => {
+      this.cardEdit.updateCard(this.card).subscribe(data => {
+          if (data.success) {
+            AlertService.newBasicAlert('Change saved successfully!', false);
+            this.toggleEditing();
+          } else {
+            AlertService.newBasicAlert('Error: ' + data.error, true);
+          }
+        },
+        failure => {
+          AlertService.newBasicAlert('Connection Error: ' + failure.message + ' (Try Again)', true);
+        });
+    });
+
+    AlertService.newAlert(newAlert);
   }
 }
