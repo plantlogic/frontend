bdec06c0c6c638a1c4974499308d648e9949024b
William Hutt <whutt1994@gmail.com>
2019-04-20 13:38:21 -0700

    Working on adding date range filter
    - added date range
    - working on lining up from and to in a single line
    - will add ranch name and commodity filter afterwards

--- src/app/card/management/export/export-card-data.component.ts
+++ src/app/card/management/export/export-card-data.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {CardExportService} from '../../../_api/card-export.service';
+import {FlatpickrOptions} from 'ng2-flatpickr';
 
 @Component({
   selector: 'app-export',
@@ -9,6 +10,8 @@ import {CardExportService} from '../../../_api/card-export.service';
 export class ExportCardDataComponent implements OnInit {
 
   constructor(private cardExport: CardExportService) { }
+
+  flatpickrOptions: FlatpickrOptions = { dateFormat: 'm-d-Y', defaultDate: new Date(Date.now())};
 
   ngOnInit() {
   }
