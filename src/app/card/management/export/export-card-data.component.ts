99c830f430f0ef7cf2f9da2fadcfaaae43f8ae97
William Hutt <whutt1994@gmail.com>
2019-04-20 14:16:12 -0700

    All filters
    - All filters in single line
    - Added in Ranch Name filter
        - Names stores in array
    - Added in Commodity filter
        - Commodities stored in an array

--- src/app/card/management/export/export-card-data.component.ts
+++ src/app/card/management/export/export-card-data.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {CardExportService} from '../../../_api/card-export.service';
 import {FlatpickrOptions} from 'ng2-flatpickr';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { TitleService } from '../../../_interact/title.service';
 
 @Component({
   selector: 'app-export',
@@ -9,11 +11,19 @@ import {FlatpickrOptions} from 'ng2-flatpickr';
 })
 export class ExportCardDataComponent implements OnInit {
 
-  constructor(private cardExport: CardExportService) { }
+  constructor(private titleService: TitleService, private fb: FormBuilder, private cardExport: CardExportService) { }
 
   flatpickrOptions: FlatpickrOptions = { dateFormat: 'm-d-Y', defaultDate: new Date(Date.now())};
-
+  form: FormGroup;
+  ranchList: Array<any> = ['Mesa Ranch', 'Wolf Ranch', 'El Paso Ranch'];
+  commodityList: Array<any> = [
+  { commodity: 'Lettuce', variety: ['Select', 'Lettuce v1', 'Lettuce v2'] },
+  { commodity: 'Strawberry', variety: ['Select', 'Strawberry v1', 'Strawberry v2'] },
+  { commodity: 'Broccoli', variety: ['Select', 'Broccoli v1', ' Broccoli v2', 'Broccoli v3'] },
+  { commodity: 'Tomato', variety: ['Select', 'Tomato v1', 'Tomato v2', 'Tomato v3'] },
+  ];
   ngOnInit() {
+    this.titleService.setTitle('Export Data');
   }
 
   generateExampleCard(): void {
