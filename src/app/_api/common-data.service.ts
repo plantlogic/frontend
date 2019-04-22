bcdd10debd462ffcaa12514ef19de2921dd08413
Matt Webb <hello@mattwebb.io>
2019-04-22 16:41:07 -0700

    Fix types

--- src/app/_api/common-data.service.ts
+++ src/app/_api/common-data.service.ts
@@ -17,12 +17,12 @@ export class CommonDataService {
     return this.http.get<BasicDTO<Array<CommonData<any>>>>(environment.ApiUrl + '/data/common', this.httpOptions);
   }
 
-  public getByKey(key: string): Observable<BasicDTO<Array<CommonData<any>>>> {
-    return this.http.get<BasicDTO<Array<CommonData<any>>>>(environment.ApiUrl + '/data/common/' + key, this.httpOptions);
+  public getByKey(key: string): Observable<BasicDTO<CommonData<any>>> {
+    return this.http.get<BasicDTO<CommonData<any>>>(environment.ApiUrl + '/data/common/' + key, this.httpOptions);
   }
 
-  public updateByKey(data: CommonData<any>): Observable<BasicDTO<Array<CommonData<any>>>> {
-    return this.http.put<BasicDTO<Array<CommonData<any>>>>(environment.ApiUrl + '/admin/common', data, this.httpOptions);
+  public updateByKey(data: CommonData<any>): Observable<BasicDTO<null>> {
+    return this.http.put<BasicDTO<null>>(environment.ApiUrl + '/admin/common', data, this.httpOptions);
   }
 }
 
