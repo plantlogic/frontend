7595f5566ff3600381eebb017ef103fac4912e4b
William Hutt <whutt1994@gmail.com>
2019-04-24 13:11:32 -0700

    cleaning up p4
    - fixed the last issue from travis
    -  be happy dangit

--- src/app/card/management/export/export-card-data.component.ts
+++ src/app/card/management/export/export-card-data.component.ts
@@ -13,7 +13,7 @@ import { TitleService } from '../../../_interact/title.service';
 export class ExportCardDataComponent implements OnInit {
 
   constructor(private titleService: TitleService, private fb: FormBuilder, private cardExport: CardExportService,
-   public commonData: CommonFormDataService) { }
+              public commonData: CommonFormDataService) { }
 
   flatpickrOptions: FlatpickrOptions = { dateFormat: 'm-d-Y', defaultDate: new Date(Date.now())};
   flatpickrOptions2: FlatpickrOptions = { dateFormat: 'm-d-Y'};
