6f2d3e66499e2a5e6a3686df51ccf3ce0f70756b
William Hutt <whutt1994@gmail.com>
2019-04-24 12:51:53 -0700

    Added in ranches from database
    - fixed layout to match the general theme
    - ranches filter now pulls from the database
    - commodities filter is still hardcoded

--- src/app/card/management/export/export-card-data.component.ts
+++ src/app/card/management/export/export-card-data.component.ts
@@ -1,3 +1,4 @@
+import { CommonFormDataService } from './../../../_api/common-form-data.service';
 import { Component, OnInit } from '@angular/core';
 import {CardExportService} from '../../../_api/card-export.service';
 import {FlatpickrOptions} from 'ng2-flatpickr';
@@ -11,7 +12,8 @@ import { TitleService } from '../../../_interact/title.service';
 })
 export class ExportCardDataComponent implements OnInit {
 
-  constructor(private titleService: TitleService, private fb: FormBuilder, private cardExport: CardExportService) { }
+  constructor(private titleService: TitleService, private fb: FormBuilder, private cardExport: CardExportService,
+    public commonData: CommonFormDataService) { }
 
   flatpickrOptions: FlatpickrOptions = { dateFormat: 'm-d-Y', defaultDate: new Date(Date.now())};
   flatpickrOptions2: FlatpickrOptions = { dateFormat: 'm-d-Y'};
@@ -20,7 +22,6 @@ export class ExportCardDataComponent implements OnInit {
   disabled = false;
   ShowFilter = false;
   limitSelection = false;
-  ranches: Array = [];
   commodities: Array = [];
   selectedItemsRanch: Array = [];
   selectedItemsCommodity: Array = [];
@@ -29,14 +30,6 @@ export class ExportCardDataComponent implements OnInit {
 
   ngOnInit() {
     this.titleService.setTitle('Export Data');
-    this.ranches = [
-      { item_id: 1, item_text: 'Del Monte' },
-      { item_id: 2, item_text: 'Baillie' },
-      { item_id: 3, item_text: 'Broome' },
-      { item_id: 4, item_text: 'Buena Vista' },
-      { item_id: 5, item_text: 'Home Canyon' },
-      { item_id: 6, item_text: 'Toro' }
-    ];
 
     this.commodities = [
       {item_id: 1, item_text: 'Lettuce'},
