513a257917c99ad48265b40116557c4ed1423624
William Hutt <whutt1994@gmail.com>
2019-04-24 12:35:42 -0700

    Added multi-select to ranches filter
    -multi-select hardcoded version works for ranches
    - starting to implement it for commodities

--- src/app/card/management/export/export-card-data.component.ts
+++ src/app/card/management/export/export-card-data.component.ts
@@ -15,20 +15,56 @@ export class ExportCardDataComponent implements OnInit {
 
   flatpickrOptions: FlatpickrOptions = { dateFormat: 'm-d-Y', defaultDate: new Date(Date.now())};
   flatpickrOptions2: FlatpickrOptions = { dateFormat: 'm-d-Y'};
-  form: FormGroup;
-  ranchList: Array<any> = ['Mesa Ranch', 'Wolf Ranch', 'El Paso Ranch', 'Coyote Ranch'];
-  commodityList: Array<any> = [
-  { commodity: 'Lettuce', variety: ['Select', 'Lettuce v1', 'Lettuce v2'] },
-  { commodity: 'Strawberry', variety: ['Select', 'Strawberry v1', 'Strawberry v2'] },
-  { commodity: 'Broccoli', variety: ['Select', 'Broccoli v1', ' Broccoli v2', 'Broccoli v3'] },
-  { commodity: 'Tomato', variety: ['Select', 'Tomato v1', 'Tomato v2', 'Tomato v3'] },
-  ];
+  myForm: FormGroup;        disabled = false;
+  ShowFilter = false;
+  limitSelection = false;
+  ranches: Array = [];
+  selectedItems: Array = [];
+  dropdownSettings: any = {};
+  commodities: Array = [];
+
   ngOnInit() {
     this.titleService.setTitle('Export Data');
+    this.ranches = [
+      { item_id: 1, item_text: 'Del Monte' },
+      { item_id: 2, item_text: 'Baillie' },
+      { item_id: 3, item_text: 'Broome' },
+      { item_id: 4, item_text: 'Buena Vista' },
+      { item_id: 5, item_text: 'Home Canyon' },
+      { item_id: 6, item_text: 'Toro' }
+    ];
+
+    this.dropdownSettings = {
+        singleSelection: false,
+        idField: 'item_id',
+        textField: 'item_text',
+        selectAllText: 'Select All',
+        unSelectAllText: 'UnSelect All',
+        itemsShowLimit: 3,
+        allowSearchFilter: this.ShowFilter
+    };
+    this.myForm = this.fb.group({
+        ranch: [this.selectedItems]
+    });
+  }
+  onItemSelect(item: any) {
+    console.log('onItemSelect', item);
+  }
+  onSelectAll(items: any) {
+      console.log('onSelectAll', items);
+  }
+  toogleShowFilter() {
+      this.ShowFilter = !this.ShowFilter;
+      this.dropdownSettings = Object.assign({}, this.dropdownSettings, { allowSearchFilter: this.ShowFilter });
   }
 
-  generateExampleCard(): void {
-    this.cardExport.exampleGenerate();
+  handleLimitSelection() {
+      if (this.limitSelection) {
+          this.dropdownSettings = Object.assign({}, this.dropdownSettings, { limitSelection: 2 });
+      } else {
+          this.dropdownSettings = Object.assign({}, this.dropdownSettings, { limitSelection: null });
+      }
   }
+
 
 }
