0354c106bc7936ad871db095002739a3a8b6d3e3
Nolan Guzman <nolanguzman@iMac.local>
2019-05-02 12:27:53 -0700

    lint may be good now

--- src/app/_api/card-export.service.ts
+++ src/app/_api/card-export.service.ts
@@ -30,67 +30,67 @@ export class CardExportService {
           // Add column labels
           table.push(['', '', '', '', '',
                       '', '', '', '', '',
-                      '1st', 'Irrigation', '', '', '', '', '', '', '', 
-                      '2nd', 'Irrigation', '', '', '', '', '', '', '', 
+                      '1st', 'Irrigation', '', '', '', '', '', '', '',
+                      '2nd', 'Irrigation', '', '', '', '', '', '', '',
                       '3rd', 'Irrigation', '', '', '', '', '', '', '',
-                      '4th', 'Irrigation', '', '', '', '', '', '', '', 
-                      '5th', 'Irrigation', '', '', '', '', '', '', '', 
+                      '4th', 'Irrigation', '', '', '', '', '', '', '',
+                      '5th', 'Irrigation', '', '', '', '', '', '', '',
                       '6th', 'Irrigation', '', '', '', '', '', '', '',
-                      '7th', 'Irrigation', '', '', '', '', '', '', '', 
-                      '8th', 'Irrigation', '', '', '', '', '', '', '', 
+                      '7th', 'Irrigation', '', '', '', '', '', '', '',
+                      '8th', 'Irrigation', '', '', '', '', '', '', '',
                       '9th', 'Irrigation', '', '', '', '', '', '', '',
-                      '10th', 'Irrigation', '', '', '', '', '', '', '', 
-                      '11th', 'Irrigation', '', '', '', '', '', '', '', 
+                      '10th', 'Irrigation', '', '', '', '', '', '', '',
+                      '11th', 'Irrigation', '', '', '', '', '', '', '',
                       '12th', 'Irrigation', '', '', '', '', '', '', '',
-                      '1st', 'Tractor', '', '', '', '', '', '', '', '', 
-                      '2nd', 'Tractor', '', '', '', '', '', '', '', '', 
+                      '1st', 'Tractor', '', '', '', '', '', '', '', '',
+                      '2nd', 'Tractor', '', '', '', '', '', '', '', '',
                       '3rd', 'Tractor', '', '', '', '', '', '', '', '',
-                      '4th', 'Tractor', '', '', '', '', '', '', '', '', 
-                      '5th', 'Tractor', '', '', '', '', '', '', '', '', 
+                      '4th', 'Tractor', '', '', '', '', '', '', '', '',
+                      '5th', 'Tractor', '', '', '', '', '', '', '', '',
                       '6th', 'Tractor', '', '', '', '', '', '', '', '',
-                      '7th', 'Tractor', '', '', '', '', '', '', '', '', 
-                      '8th', 'Tractor', '', '', '', '', '', '', '', '', 
+                      '7th', 'Tractor', '', '', '', '', '', '', '', '',
+                      '8th', 'Tractor', '', '', '', '', '', '', '', '',
                       '9th', 'Tractor', '', '', '', '', '', '', '', '',
-                      '10th', 'Tractor', '', '', '', '', '', '', '', '', 
-                      '11th', 'Tractor', '', '', '', '', '', '', '', '', 
+                      '10th', 'Tractor', '', '', '', '', '', '', '', '',
+                      '11th', 'Tractor', '', '', '', '', '', '', '', '',
                       '12th', 'Tractor', '', '', '', '', '', '', '', '',
-                      '1st', 'Commodity', '', '', '', '', '', 
-                      '2nd', 'Commodity', '', '', '', '', '', 
+                      '1st', 'Commodity', '', '', '', '', '',
+                      '2nd', 'Commodity', '', '', '', '', '',
                       '3rd', 'Commodity', '', '', '', '', '',
-                      '1st', 'Pre Plant', '', '', '', '', '', '', 
-                      '2nd', 'Pre Plant', '', '', '', '', '', '', 
+                      '1st', 'Pre Plant', '', '', '', '', '', '',
+                      '2nd', 'Pre Plant', '', '', '', '', '', '',
                       '3rd', 'Pre Plant', '', '', '', '', '', '',
-                      '1st', 'At Plant', '', '', '', '', '', '', 
-                      '2nd', 'At Plant', '', '', '', '', '', '', 
+                      '1st', 'At Plant', '', '', '', '', '', '',
+                      '2nd', 'At Plant', '', '', '', '', '', '',
                       '3rd', 'At Plant', '', '', '', '', '', '',
           ]);
           table.push(['Field ID', 'Ranch Name', 'Ranch Manager', 'Lot Number', 'Shipper ID',
                       'Wet Date', 'Thin Date', 'Hoe Date', 'Harvest Date', '',
                       // Irrigation Data, 12 total
-                      'Date', 'Method', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '', 
                       'Date', 'Method', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '',
-                      'Date', 'Method', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '', 
                       'Date', 'Method', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '',
-                      'Date', 'Method', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '', 
                       'Date', 'Method', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '',
-                      'Date', 'Method', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '', 
                       'Date', 'Method', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '',
-                      'Date', 'Method', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '', 
                       'Date', 'Method', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '',
-                      'Date', 'Method', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '', 
+                      'Date', 'Method', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '',
+                      'Date', 'Method', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '',
+                      'Date', 'Method', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '',
+                      'Date', 'Method', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '',
+                      'Date', 'Method', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '',
+                      'Date', 'Method', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '',
                       'Date', 'Method', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '',
                       // Tractor Data, 12 total
-                      'Date', 'Work Done', 'Operator', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '', 
                       'Date', 'Work Done', 'Operator', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '',
-                      'Date', 'Work Done', 'Operator', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '', 
                       'Date', 'Work Done', 'Operator', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '',
-                      'Date', 'Work Done', 'Operator', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '', 
                       'Date', 'Work Done', 'Operator', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '',
-                      'Date', 'Work Done', 'Operator', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '', 
                       'Date', 'Work Done', 'Operator', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '',
-                      'Date', 'Work Done', 'Operator', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '', 
                       'Date', 'Work Done', 'Operator', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '',
-                      'Date', 'Work Done', 'Operator', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '', 
+                      'Date', 'Work Done', 'Operator', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '',
+                      'Date', 'Work Done', 'Operator', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '',
+                      'Date', 'Work Done', 'Operator', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '',
+                      'Date', 'Work Done', 'Operator', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '',
+                      'Date', 'Work Done', 'Operator', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '',
+                      'Date', 'Work Done', 'Operator', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '',
                       'Date', 'Work Done', 'Operator', 'Chemical', 'Rate', 'Unit', 'Fertilizer', 'Rate', 'Unit', '',
                       // Commodity Data, 3 Total
                       'Commodity', 'Crop Acres', 'Bed Type', 'Bed Count', 'SeedLotNumber', 'Variety', '',
@@ -136,32 +136,33 @@ export class CardExportService {
                 x.irrigationArray.forEach(y => {
                   if (!y.chemical) {
                     if (!y.fertilizer) {
-
                       dataLine.push('', String(y.workDate), y.method, '', '', '', '', '', '');
-                    } else{
-                      dataLine.push('', String(y.workDate), y.method, '', '', '', y.fertilizer.name, String(y.fertilizer.rate), String(y.fertilizer.unit));
+                    } else {
+                      dataLine.push('', String(y.workDate), y.method, '', '', '',
+                                    y.fertilizer.name, String(y.fertilizer.rate), String(y.fertilizer.unit));
                     }
-
                   } else if (!y.fertilizer) {
                     if (!y.chemical) {
                       dataLine.push('', String(y.workDate), y.method, '', '', '', '', '', '');
                     } else {
-                      dataLine.push('', String(y.workDate), y.method, y.chemical.name, String(y.chemical.rate), String(y.chemical.unit), '', '', '');
+                      dataLine.push('', String(y.workDate), y.method,
+                                    y.chemical.name, String(y.chemical.rate), String(y.chemical.unit), '', '', '');
                     }
                   } else {
-                     dataLine.push('', String(y.workDate), y.method, y.chemical.name, String(y.chemical.rate), String(y.chemical.unit),
+                     dataLine.push('', String(y.workDate), y.method, y.chemical.name,
+                                   String(y.chemical.rate), String(y.chemical.unit),
                                    y.fertilizer.name, String(y.fertilizer.rate), String(y.fertilizer.unit));
                   }
                   pushCounter += 1;
                 });
-                if (pushCounter<12) {
-                  while (pushCounter<12) {
+                if (pushCounter < 12) {
+                  while (pushCounter < 12) {
                     dataLine.push('', '', '', '', '', '', '', '', '');
                     pushCounter += 1;
                   }
                 }
                 pushCounter = 0;
-              } 
+              }
               // Retrieve data from nested tractor data objects and insert into table
 
               if (!x.tractorArray.length) {
@@ -183,29 +184,31 @@ export class CardExportService {
                     if (!z.fertilizer) {
                       dataLine.push('', String(z.workDate), z.workDone, z.operator, '', '', '', '', '', '');
                     } else {
-                      dataLine.push('', String(z.workDate), z.workDone, z.operator, '', '', '', z.fertilizer.name, String(z.fertilizer.rate), String(z.fertilizer.unit));
+                      dataLine.push('', String(z.workDate), z.workDone, z.operator, '', '', '',
+                                    z.fertilizer.name, String(z.fertilizer.rate), String(z.fertilizer.unit));
                     }
                   } else if (!z.fertilizer) {
-                    if( !z.chemical) {
+                    if (!z.chemical) {
                       dataLine.push('', String(z.workDate), z.workDone, z.operator, '', '', '', '', '', '');
                     } else {
-                      dataLine.push('', String(z.workDate), z.workDone, z.operator, z.chemical.name, String(z.chemical.rate), String(z.chemical.unit), '', '', '');
+                      dataLine.push('', String(z.workDate), z.workDone, z.operator, z.chemical.name,
+                                    String(z.chemical.rate), String(z.chemical.unit), '', '', '');
                     }
                   } else {
-                     dataLine.push('', String(z.workDate), z.workDone, z.operator, z.chemical.name, String(z.chemical.rate), String(z.chemical.unit),
+                     dataLine.push('', String(z.workDate), z.workDone, z.operator, z.chemical.name,
+                                   String(z.chemical.rate), String(z.chemical.unit),
                                    z.fertilizer.name, String(z.fertilizer.rate), String(z.fertilizer.unit));
                   }
                   pushCounter += 1;
                 });
-                if (pushCounter<12) {
-                  while (pushCounter<12) {
+                if (pushCounter < 12) {
+                  while (pushCounter < 12) {
                     dataLine.push('', '', '', '', '', '', '', '', '', '');
                     pushCounter += 1;
                   }
                 }
                 pushCounter = 0;
               }
-    
               if (!x.commodityArray.length) {
                 dataLine.push('', '', '', '', '', '', '');
                 dataLine.push('', '', '', '', '', '', '');
@@ -216,15 +219,14 @@ export class CardExportService {
                                 String(c.bedCount), String(c.seedLotNumber), c.variety);
                   pushCounter += 1;
                 });
-                if (pushCounter<3) {
-                  while (pushCounter<3) {
+                if (pushCounter < 3) {
+                  while (pushCounter < 3) {
                     dataLine.push('', '', '', '', '', '', '');
                     pushCounter += 1;
                   }
                 }
                 pushCounter = 0;
               }
-              
               if (!x.preChemicalArray.length) {
                 dataLine.push('', '', '', '', '', '', '', '');
                 dataLine.push('', '', '', '', '', '', '', '');
@@ -235,23 +237,25 @@ export class CardExportService {
                     if (!preC.fertilizer) {
                       dataLine.push('', String(preC.date), '', '', '', '', '', '');
                     } else {
-                      dataLine.push('', String(preC.date), '', '', '', preC.fertilizer.name, String(preC.fertilizer.rate), String(preC.fertilizer.unit));
+                      dataLine.push('', String(preC.date), '', '', '', preC.fertilizer.name,
+                                    String(preC.fertilizer.rate), String(preC.fertilizer.unit));
                     }
-                  }
-                  else if (!preC.fertilizer) {
+                  } else if (!preC.fertilizer) {
                     if (!preC.chemical) {
                       dataLine.push('', String(preC.date), '', '', '', '', '', '');
                     } else {
-                      dataLine.push('', String(preC.date), preC.chemical.name, String(preC.chemical.rate), String(preC.chemical.unit), '', '', '');
+                      dataLine.push('', String(preC.date), preC.chemical.name, String(preC.chemical.rate),
+                                    String(preC.chemical.unit), '', '', '');
                     }
                   } else {
-                     dataLine.push('', String(preC.date), preC.chemical.name, String(preC.chemical.rate), String(preC.chemical.unit),
+                     dataLine.push('', String(preC.date), preC.chemical.name,
+                                   String(preC.chemical.rate), String(preC.chemical.unit),
                                    preC.fertilizer.name, String(preC.fertilizer.rate), String(preC.fertilizer.unit));
                   }
                   pushCounter += 1;
                 });
-                if (pushCounter<3) {
-                  while (pushCounter<3) {
+                if (pushCounter < 3) {
+                  while (pushCounter < 3) {
                     dataLine.push('', '', '', '', '', '', '', '');
                     pushCounter += 1;
                   }
@@ -267,23 +271,25 @@ export class CardExportService {
                     if (!postC.fertilizer) {
                       dataLine.push('', String(postC.date), '', '', '', '', '', '');
                     } else {
-                      dataLine.push('', String(postC.date), '', '', '', postC.fertilizer.name, String(postC.fertilizer.rate), String(postC.fertilizer.unit));
+                      dataLine.push('', String(postC.date), '', '', '', postC.fertilizer.name,
+                                    String(postC.fertilizer.rate), String(postC.fertilizer.unit));
                     }
-                  }
-                  else if (!postC.fertilizer) {
+                  } else if (!postC.fertilizer) {
                     if (!postC.chemical) {
                       dataLine.push('', String(postC.date), '', '', '', '', '', '');
                     } else {
-                      dataLine.push('', String(postC.date), postC.chemical.name, String(postC.chemical.rate), String(postC.chemical.unit), '', '', '');
+                      dataLine.push('', String(postC.date), postC.chemical.name,
+                                    String(postC.chemical.rate), String(postC.chemical.unit), '', '', '');
                     }
                   } else {
-                     dataLine.push('', String(postC.date), postC.chemical.name, String(postC.chemical.rate), String(postC.chemical.unit),
+                     dataLine.push('', String(postC.date), postC.chemical.name,
+                                   String(postC.chemical.rate), String(postC.chemical.unit),
                                    postC.fertilizer.name, String(postC.fertilizer.rate), String(postC.fertilizer.unit));
                   }
                   pushCounter += 1;
                 });
-                if (pushCounter<3) {
-                  while (pushCounter<3) {
+                if (pushCounter < 3) {
+                  while (pushCounter < 3) {
                     dataLine.push('', '', '', '', '', '', '', '');
                     pushCounter += 1;
                   }
