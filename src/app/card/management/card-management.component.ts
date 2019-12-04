aa50f336d4bc1a8994471bf31f0f15e68769c019
ChristopherLeeWilliams <chris_lw16@yahoo.com>
2019-12-03 19:22:09 -0800

    Added inline editing of fieldID, no css added yet

--- src/app/card/management/card-management.component.ts
+++ src/app/card/management/card-management.component.ts
@@ -3,6 +3,7 @@ import {Card} from '../../_dto/card/card';
 import {AlertService} from '../../_interact/alert/alert.service';
 import {TitleService} from '../../_interact/title.service';
 import {CardViewService} from '../../_api/card-view.service';
+import {CardEditService} from '../../_api/card-edit.service';
 import {MdbTableService} from 'angular-bootstrap-md';
 import {NavService} from '../../_interact/nav.service';
 import {AuthService} from '../../_auth/auth.service';
@@ -14,8 +15,8 @@ import {PlRole} from '../../_dto/user/pl-role.enum';
   styleUrls: ['./card-management.component.scss']
 })
 export class CardManagementComponent implements OnInit {
-  constructor(private titleService: TitleService, private cardService: CardViewService, private tableService: MdbTableService,
-              private nav: NavService, private auth: AuthService) { }
+  constructor(private titleService: TitleService, private cardService: CardViewService, private cardEdit: CardEditService,
+              private tableService: MdbTableService, private nav: NavService, private auth: AuthService) { }
 
   cards: Card[] = [];
   filter: string;
@@ -25,8 +26,6 @@ export class CardManagementComponent implements OnInit {
     this.titleService.setTitle('All Cards');
     this.loadCardData();
   }
-
-
 
   private loadCardData() {
     this.cardService.getAllCards().subscribe(
@@ -60,6 +59,22 @@ export class CardManagementComponent implements OnInit {
       this.cards = this.tableService.searchLocalDataBy(this.filter.toLowerCase());
       this.tableService.setDataSource(prev);
     }
+  }
+
+  private updateFieldId(c: Card): void {
+    console.log(c);
+    console.log("updating Field Id");
+    this.cardEdit.updateCard(c).subscribe(data => {
+      if (data.success) {
+        AlertService.newBasicAlert('Change saved successfully!', false);
+        this.loadCardData();
+      } else {
+        AlertService.newBasicAlert('Error: ' + data.error, true);
+      }
+    },
+    failure => {
+      AlertService.newBasicAlert('Connection Error: ' + failure.message + ' (Try Again)', true);
+    });
   }
 
   // Used for animation
